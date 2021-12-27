3c124499eca7698342ca9a29f7557e1fbc67e262
ddrago0o36 <drago0o036@gmail.com>
2021-12-27 16:54:19 +0200

    ac

--- app/src/main/java/com/example/fast_food_app/MainActivity.java
+++ app/src/main/java/com/example/fast_food_app/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.fast_food_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Login(View view) {
+        Intent i = new Intent();
+        i.setClass(getApplicationContext(), MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getApplicationContext().startActivity(i);
+    }
+    public void Register(View view){
+        Intent i = new Intent();
+        i.setClass(getApplicationContext(), RegisterActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getApplicationContext().startActivity(i);
     }
 }
